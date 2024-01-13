cd859a44492969db5ea668a3dbf15d0ada94af74
ash2code <ashoksoma95@gmail.com>
2024-01-13 18:13:38 +0530

    my java code

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+// Define the HelloWorld class
+public class HelloWorld {
+
+    // Main method to execute the application
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
